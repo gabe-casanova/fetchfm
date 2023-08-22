f06e1945e0f64f799518fcaa256ce7de9620cbac
Gabriel Casanova <gabecasanova7@gmail.com>
2023-08-22 17:29:08 -0500

    updated print_bytey_welcome_msg()

--- v3/fetchfm.py
+++ v3/fetchfm.py
@@ -1,10 +1,11 @@
+from time import sleep
 from os import path
 from ansi import ANSI
 from catalog import Catalog
 from api_handler import get_path, fetch_scrobbled_data, is_valid_user
 from menu_choices import MainMenuChoices
 
-# TODO-- when requesting data for Last.fm user "make" it causes a DivideByZero Error in print_user_info()
+# TODO-- when requesting data for Last.fm user "make" it causes a DivideByZero Error in print_fun_facts()
 
 LOGO = """
       ███████╗███████╗████████╗░█████╗░██╗░░██╗░░░███████╗███╗░░░███╗  _ ♪   /)---(\\
@@ -22,7 +23,8 @@ username = ''
 # =========== [1] Main Program: =============================================
 
 def main():
-    prompt_for_username()  # sets global username variable
+    display_logo()
+    get_username()
     if username == '':
         # the user has decided to default to the current user
         found, error_msg = get_default_user()
@@ -44,26 +46,25 @@ def main():
 
 
 def run_fetchfm():
-    create_database()
-    ''' dictionary mapping menu choices to corresponding helper functions '''
-    CHOICE_FUNCTIONS = {
-        MainMenuChoices.USER_INFO: print_user_info,
+    MENU_FUNCTIONS = {
+        MainMenuChoices.USER_INFO: print_fun_facts,
         MainMenuChoices.TIME_STATS: NotImplemented,
         MainMenuChoices.TRACK_STATS: NotImplemented,
         MainMenuChoices.PRINTING: NotImplemented
     }
+    create_database()
     print_bytey_welcome_msg()
     while True:
         display_main_menu()
         my_choice = get_main_menu_choice()
         if my_choice == MainMenuChoices.QUIT:
             break
-        CHOICE_FUNCTIONS[my_choice]()  # call corresponding function (switch)
+        MENU_FUNCTIONS[my_choice]()  # call corresponding function (switch)
     # if we get here, the user has terminated the program
     print(f'Thanks for using {ANSI.BRIGHT_CYAN_BOLD}Fetch.fm{ANSI.RESET}!\n')
 
 
-def print_user_info():
+def print_fun_facts():
     '''
     Prints Bytey's fun facts (i.e. user info) to the terminal
     '''
@@ -79,50 +80,56 @@ def print_user_info():
     user = realname if realname is not None else username
     # format the output with ANSI and commas
     ansi_user = f'{ANSI.CYAN_BOLD}{user}{ANSI.RESET}'
-    ansi_playcount = ansi_commas(playcount)
-    ansi_track_count = ansi_commas(track_count)
-    ansi_album_count = ansi_commas(album_count)
-    ansi_artist_count = ansi_commas(artist_count)
+    ansi_playcount = ansi_with_commas(playcount)
+    ansi_track_count = ansi_with_commas(track_count)
+    ansi_album_count = ansi_with_commas(album_count)
+    ansi_artist_count = ansi_with_commas(artist_count)
     # calculate daily average and format w ANSI
     daily_average = round(playcount / total_num_days)
-    ansi_daily_average = ansi_commas(daily_average)
-    ansi_num_days = ansi_commas(total_num_days)
+    ansi_daily_average = ansi_with_commas(daily_average)
+    ansi_num_days = ansi_with_commas(total_num_days)
     ''' Get Bytey the ASCII dog broken up into its individual lines '''
-    BYTEY = get_bytey_ansi_parts(ANSI.BRIGHT_CYAN_BOLD, True)
+    BYTEY = get_ansi_bytey(ANSI.BRIGHT_CYAN_BOLD, True)
     # print the message to the screen
-    print(f'\n{BYTEY[0]}')
-    print(f'{BYTEY[1]}Hey there, {ansi_user}! Here are some fun music stats I '
+    print(f'\n   {BYTEY[0]}')
+    print(f'   {BYTEY[1]}Hey there, {ansi_user}! Here are some fun music stats I '
           'dug up about you--')
-    print(f'{BYTEY[2]} ⇒ you\'ve listened to {ansi_playcount} tracks over the '
+    print(f'   {BYTEY[2]} ⇒ you\'ve listened to {ansi_playcount} tracks over the '
           f'past {ansi_num_days} days, averaging {ansi_daily_average} tracks '
           'listened a day')
-    print(f'{BYTEY[3]} ⇒ you\'ve enjoyed {ansi_track_count} unique songs, '
+    print(f'   {BYTEY[3]} ⇒ you\'ve enjoyed {ansi_track_count} unique songs, '
           f'explored {ansi_album_count} different albums, and discovered '
           f'{ansi_artist_count} diverse artists')
     if num_streams == 0:
         # edge case
-        print(f'{BYTEY[4]}\n')
+        print(f'   {BYTEY[4]}\n')
         return
     # if we get here, we know most_streamed_days list is NOT empty
     formatted_date = most_streamed_days[0].strftime('%B %d, %Y')
     ansi_date = f'{ANSI.CYAN_BOLD}{formatted_date}{ANSI.RESET}'
-    ansi_num_streams = ansi_commas(num_streams)
-    print(f'{BYTEY[4]} ⇒ on {ansi_date} you listened to a lot of music, with '
+    ansi_num_streams = ansi_with_commas(num_streams)
+    print(f'   {BYTEY[4]} ⇒ on {ansi_date} you listened to a lot of music, with '
           f'{ansi_num_streams} total songs played (..the most of any day!)\n')
     
 
 # =========== [2] Miscellaneous: ============================================
 
-def prompt_for_username():
-    global username
+def display_logo():
+    '''
+    Prints the Fetch.fm logo to the screen
+    '''
     ansi_logo = f'{ANSI.BRIGHT_CYAN_BOLD}{LOGO}{ANSI.RESET}'
-    ansi_lastfm = f'{ANSI.CYAN_BOLD}Last.fm username{ANSI.RESET}'
-    ansi_enter = f'{ANSI.BRIGHT_WHITE_BOLD}`enter`{ANSI.RESET}'
-    print(f'\n{ansi_logo}\n')
-    print(f'Enter your {ansi_lastfm} or press {ansi_enter} to default to '
-          f'the current user: {ANSI.BRIGHT_GREEN}', end='')
+    print_text_animated(f'\n{ansi_logo}\n\n')
+
+
+def get_username():
+    global username
+    ANSI_LASTFM = f'{ANSI.CYAN_BOLD}Last.fm username{ANSI.RESET}'
+    ANSI_ENTER = f'{ANSI.BRIGHT_WHITE_BOLD}`enter`{ANSI.RESET}'
+    print(f'Enter your {ANSI_LASTFM} or press {ANSI_ENTER} to default to '
+          f'the current user: {ANSI.BRIGHT_CYAN}', end='')
     username = input()
-    print(ANSI.RESET, end='')
+    print(ANSI.RESET, end='')  # resets ansi back to default
 
 
 def get_default_user():
@@ -137,7 +144,7 @@ def get_default_user():
             username = f.read()
         return True, ''
     except (FileNotFoundError, OSError):
-        error_header = ' * ERROR: Default failed'
+        error_header = ' * No current user found'
         bullet_1 = ('-> Please ensure you\'ve ran `python api_handler.py '
                     'getdata` before you attempt to run `python fetchfm.py`')
         bullet_2 = ('-> Or, please enter your Last.fm username instead of '
@@ -155,6 +162,42 @@ def create_database():
     with open(file_path, 'r') as f:
         scrobbled_data = f.readlines()
         db = Catalog(username, scrobbled_data)
+
+
+def display_main_menu():
+    '''
+    Prints the Main Menu choices to the terminal for the user to read
+    '''
+    print(f'\n {ANSI.BRIGHT_CYAN_BOLD}Main Menu:{ANSI.RESET}')
+    display_option(1, 'Fetch\'s fun facts ♪♪♪')
+    display_option(2, 'explore your timing data')
+    display_option(3, 'display track-based stats')
+    display_option(4, 'delve into your scrobbles')
+    display_option(5, 'quit program\n')
+
+
+def display_option(num, choice_description):
+    '''
+    Formats and prints the given menu-choice selection
+    '''
+    print(f'  ‣ {ANSI.BRIGHT_CYAN}{num}{ANSI.RESET}  {choice_description}')
+
+
+def get_main_menu_choice():
+    '''
+    Retrieves the user's Main Menu choice (as an int) from the terminal
+    '''
+    MENU_CHOICES = list(MainMenuChoices)
+    ansi_enter = f' {ANSI.BRIGHT_CYAN_BOLD}Enter Option #:{ANSI.RESET} '
+    while True:
+        user_input = input(f'{ansi_enter}')
+        if user_input.isdigit() and 1 <= int(user_input) <= len(MENU_CHOICES):
+            print()
+            break
+        # if we get here, the user did not type in an int. display message
+        ansi_input = f'{ANSI.BRIGHT_CYAN_BOLD}{user_input}{ANSI.RESET}'
+        print(f'\n  * {ansi_input} isn\'t a valid choice\n')
+    return MENU_CHOICES[int(user_input) - 1]
 
 
 def read_user_info_txt_file():
@@ -195,43 +238,26 @@ def read_user_info_txt_file():
         return None
 
 
-def get_main_menu_choice():
-    '''
-    Retrieves the user's Main Menu choice (as an int) from the terminal
-    '''
-    # TODO-- bug can't rerun option 1
-    MENU_CHOICES = list(MainMenuChoices)
-    ansi_enter = f' {ANSI.BRIGHT_GREEN_BOLD}Enter Option #:{ANSI.RESET} '
-    while True:
-        user_input = input(f'{ansi_enter}')
-        if user_input.isdigit() and 1 <= int(user_input) <= len(MENU_CHOICES):
-            print()
-            break
-        # if we get here, the user did not type in an int. display message
-        ansi_input = f'{ANSI.BRIGHT_CYAN_BOLD}{user_input}{ANSI.RESET}'
-        print(f'\n  * {ansi_input} isn\'t a valid choice\n')
-    return MENU_CHOICES[int(user_input) - 1]
-
-
 def print_bytey_welcome_msg():
-    BYTEY = get_bytey_ansi_parts(ANSI.BRIGHT_CYAN_BOLD, False)
+    BYTEY = get_ansi_bytey(ANSI.BRIGHT_CYAN_BOLD, False)
     msg = f"""
-  /‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\\
- |                               Welcome to Fetch.fm!                                   |
- |                                                                                      |
- |   My name's Bytetholomew the Dog, but you can just call me `Fetch` for short! I'm    |
- | sorta the top dog over at Fetch.fm HQ, but I thought I'd tag along with you for now. |
- |                                                                                      |
- |    Please select one of the following menu options to get started using Fetch.fm!    |
-  \\___________________________________________   ______________________________________/
-                                              | /
-                                {BYTEY[0]} |/
-                                {BYTEY[1]}
-                                {BYTEY[2]}
-                                {BYTEY[3]}
-                                {BYTEY[4]}
+  /‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\\
+ |                              Welcome to Fetch.fm!                                  |
+ |                                                                                    |
+ |   My name's Bytetholomew, but you can call me `Fetch` for short! I'm the company   |
+ |   mascot, but today's my day off so I thought I'd spend it taging along with you   |
+ |                                                                                    |
+ |   Please select one of the following menu options to get started using Fetch.fm!   |
+  \\__________________________________________   _____________________________________/
+                                             | /
+                               {BYTEY[0]} |/
+                               {BYTEY[1]}
+                               {BYTEY[2]}
+                               {BYTEY[3]}
+                               {BYTEY[4]}
 """
-    print(f'\n{msg}', end='')
+    print_text_animated(f'\n{msg}')
+    sleep(0.25)
 
 
 # =========== [3] Utility: ==================================================
@@ -248,46 +274,39 @@ def seconds_to_days(total_seconds):
     return num_days
 
 
-def display_main_menu():
+def print_text_animated(text):
+    LENGTH = len(text)
+    for i in range(LENGTH):
+        if text[i] != ' ':
+            # only sleep for visible chars
+            sleep(0.007)
+        print(text[i], end='', flush=True)
+
+
+def ansi_with_commas(num):
     '''
-    Prints the Main Menu choices to the terminal for the user to read
-    '''
-    print()
-    print(f' {ANSI.BRIGHT_GREEN_BOLD}Main Menu:{ANSI.RESET}')
-    print_menu_choice('  ', 1, 'Fetch\'s fun facts ♪♪♪')
-    print_menu_choice('  ', 2, 'timing data')
-    print_menu_choice('  ', 3, 'track-based stats')
-    print_menu_choice('  ', 4, 'delve into your scrobbles')
-    print_menu_choice('  ', 5, 'quit program')
-    print()
-
-
-def print_menu_choice(indent, key, msg):
-    print(f'{indent}‣ {ANSI.BRIGHT_GREEN}{key}{ANSI.RESET}  {msg}')
-
-
-def ansi_commas(num):
-    '''
-    Formats the given number with commas and displays it in ANSI
+    Formats the given number with commas and displays it with ANSI
     '''
     return f'{ANSI.CYAN_BOLD}{format(num, ",")}{ANSI.RESET}'
 
 
-def get_bytey_ansi_parts(ansi, trailing_aligned:bool) -> list:
-    if trailing_aligned:
-        BYTEY_1 = '_     /)---(\\   '
-        BYTEY_2 = '\\\\   (/ . . \\)  '
-        BYTEY_3 = ' \\\\__)-\\(*)/    '
-        BYTEY_4 = ' \\_       (_    '
-        BYTEY_5 = ' (___/-(____)   '
+def get_ansi_bytey(ansi, aligned) -> list:
+    '''
+    Returns a list in which each element is a particular line of ansi Bytey
+    '''
+    if aligned:
+        alignment = ['   ', '  ', '    ', '    ', '   ']
     else:
-        BYTEY_1 = '_     /)---(\\'
-        BYTEY_2 = '\\\\   (/ . . \\)'
-        BYTEY_3 = ' \\\\__)-\\(*)/'
-        BYTEY_4 = ' \\_       (_'
-        BYTEY_5 = ' (___/-(____)'
-    temp_list = [BYTEY_1, BYTEY_2, BYTEY_3, BYTEY_4, BYTEY_5]
-    ansi_bytey = [f'{ansi}{line}{ANSI.RESET}' for line in temp_list]
+        alignment = ['', '', '', '', '']
+    # construct bytey now
+    BYTEY = [
+        '_     /)---(\\' + alignment[0],
+        '\\\\   (/ . . \\)' + alignment[1],
+        ' \\\\__)-\\(*)/' + alignment[2],
+        ' \\_       (_' + alignment[3],
+        ' (___/-(____)' + alignment[4]
+    ]
+    ansi_bytey = [f'{ansi}{line}{ANSI.RESET}' for line in BYTEY]
     return ansi_bytey
 
 
